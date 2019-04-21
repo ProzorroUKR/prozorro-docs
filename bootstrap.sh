f45c635159c7baa8cf27849ae5d3b46722527a03
smithumble <smithumble@gmail.com>
2019-04-22 01:09:19 +0300

    Add travis config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 virtualenv -p python2.7 .
-#curl https://bootstrap.pypa.io/get-pip.py | ./bin/python  # fixes "There was a problem confirming the ssl certificate: [SSL: TLSV1_ALERT_PROTOCOL_VERSION] tlsv1 a..."
+
 ./bin/pip install -r requirements.txt
 
 git clone https://github.com/ProzorroUKR/openprocurement.api.git src/openprocurement.api
