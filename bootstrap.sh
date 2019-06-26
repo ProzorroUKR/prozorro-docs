e8f341c30bf96b22b18765f29c9efdf4383b9300
smithumble <smithumble@gmail.com>
2019-06-26 17:39:42 +0300

    Revert bootstrap branch

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@ virtualenv -p python2.7 venv
 
 ./venv/bin/pip install -r requirements.txt
 
-git clone https://github.com/ProzorroUKR/openprocurement.api.git -b transfer-token src/openprocurement.api
+git clone https://github.com/ProzorroUKR/openprocurement.api.git src/openprocurement.api
 ./venv/bin/pip install -r src/openprocurement.api/requirements.txt
 ./venv/bin/pip install -e src/openprocurement.api/
