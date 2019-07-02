b3754faa1f467629377f2c2494112ad858f8c194
smithumble <smithumble@gmail.com>
2019-07-02 04:40:13 +0300

    Use travis branch

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+
+REPO=https://github.com/ProzorroUKR/openprocurement.api.git
+DEST=src/openprocurement.api
+
 virtualenv -p python2.7 venv
 
 ./venv/bin/pip install -r requirements.txt
 
-git clone https://github.com/ProzorroUKR/openprocurement.api.git src/openprocurement.api
-./venv/bin/pip install -r src/openprocurement.api/requirements.txt
-./venv/bin/pip install -e src/openprocurement.api/
+git clone --branch ${BRANCH:-master} ${REPO} ${DEST} || git clone ${REPO} ${DEST}
+
+./venv/bin/pip install -r $DEST/requirements.txt
+./venv/bin/pip install -e $DEST/
